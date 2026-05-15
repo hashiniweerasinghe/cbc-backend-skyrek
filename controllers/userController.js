77483eb0a33e4215732e1e05768595928ed1487d
Hashini Weerasinghe <hashiniwrs@gmail.com>
2026-05-15 19:37:54 +0530

    add the isCustomer Validation

--- controllers/userController.js
+++ controllers/userController.js
@@ -118,3 +118,14 @@ export function isAdmin(req){
 
     return true;
 }
+
+
+export function isCustomer(req){
+    if(req.user==null){
+        return false;
+    }
+    if(req.user.role != "customer"){
+        return false;
+    }
+    return true;
+}
